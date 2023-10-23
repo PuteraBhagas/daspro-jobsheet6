84b5428f2af41a993de7a5ccc6fea9489b5778a7
PuteraBhagas <p.bhagaswara@gmail.com>
2023-10-23 14:02:13 +0700

    percobaan 1 sebelum di modifikasi

---
+++ Pemilihan2Percobaan125.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Pemilihan2Percobaan125
+ */
+public class Pemilihan2Percobaan125 {
+
+    public static void main(String[] args) {
+        Scanner input25 = new Scanner(System.in);
+        
+       
+        System.out.println("Masukan Tahun");
+        int tahun = input25.nextInt();
+
+        
+        if ((tahun % 4) == 0) {
+            
+            if ((tahun % 100) != 0);
+            System.out.println("Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+
+            
+        }
+    }
+}
