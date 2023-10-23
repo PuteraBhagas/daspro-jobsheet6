998386752cf5dcaf092c1b45bfd8ea6fabb65326
PuteraBhagas <p.bhagaswara@gmail.com>
2023-10-23 16:18:19 +0700

    Perecobaan ke 2 saya

---
+++ Pemilihan2Percobaan225.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan225 {
+    public static void main(String[] args) {
+        Scanner input25 = new Scanner(System.in);
+        float totalSudut;
+
+        System.out.println("Masukkan Sudut 1 : ");
+        float sudut1 = input25.nextInt();
+        System.out.println("Masukkan Sudut 2 : ");
+        float sudut2 = input25.nextInt();
+        System.out.println("Masukkan Sudut 3 : ");
+        float sudut3 = input25.nextInt();
+
+        totalSudut = sudut1 + sudut2 + sudut3;
+
+        if (totalSudut == 180) {
+            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
+                System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+            else 
+                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+        } else {
+            System.out.println("Bukan Segitiga");
+        }  
+}
+
+}
